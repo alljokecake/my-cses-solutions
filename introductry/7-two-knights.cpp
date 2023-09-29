af7a8df6b08fb9c0b549bbd2950a8eefb41f8503
schlanqe <ilhanburcakbas@gmail.com>
2023-09-29 20:51:03 +0300

    leftovers

--- introductry/7-two-knights.cpp
+++ introductry/7-two-knights.cpp
@@ -1,8 +1,7 @@
-// TODO!
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long;
+#define ll long long
 
 int main() {
 }
---
+++ introductry/8-two-sets.cpp
@@ -0,0 +1,14 @@
+// TODO!
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+  ll n;
+  cin >> n;
+  if(n*(n+1)/2%2) {
+    cout << "NO";
+    return 0;
+  }
+}
