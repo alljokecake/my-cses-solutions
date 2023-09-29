35e84b0722771bcd03bc265b982a373fb63c68c9
schlanqe <ilhanburcakbas@gmail.com>
2023-09-29 16:37:38 +0300

    solved 5

--- introductry/5-permutations.cpp
+++ introductry/5-permutations.cpp
@@ -1,4 +1,3 @@
-// TODO!
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -15,4 +14,20 @@ int main() {
     cout << "NO SOLUTION";
     return 0;
   }
+  
+  if(n%2==0) 
+    for(int i=0; i<=n; i+=2)
+      cout << i << " ";
+    for(int i=0; i<=n-1; i+=1)
+      cout << i << " ";
+  } else {
+    for(int i=0; i<=n; ++i)
+      cout << i << " ";
+    for(int i=0; i<=n-1; i+=2)
+      cout << i << " ";
+  }
+
+
+// 1 3 5 7 2 4 6
+// 2 4 6 1 3 5
 }
